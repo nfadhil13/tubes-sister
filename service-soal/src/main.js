ca6c7547175028e70390995465a9f817b83b9bad
RayhanAnandhias <48201126+RayhanAnandhias@users.noreply.github.com>
2021-07-24 17:36:18 +0700

    init main

---
+++ service-soal/src/main.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// error handling
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const cause = error.cause || "Internal Server Error";
+  res.status(status).json({
+    message: message,
+    error: status,
+    cause: cause
+  });
+});
+
+const startServer = () => {
+  const PORT = process.env.SERVER_PORT || "3000";
+  app.listen(PORT, () => {
+    console.log(`Service Email listening at PORT ${PORT}`);
+  });
+};
+
+startServer();
